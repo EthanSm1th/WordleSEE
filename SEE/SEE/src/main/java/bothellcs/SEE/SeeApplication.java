3143130d79ec8005912952b2e62215b954ac41d5
Evin Onener <1107756@apps.nsd.org>
2022-06-02 08:47:42 -0700

    I fixed it ya

--- SEE/SEE/src/main/java/bothellcs/SEE/SeeApplication.java
+++ SEE/SEE/src/main/java/bothellcs/SEE/SeeApplication.java
@@ -1,13 +1,24 @@
-package bothellcs.SEE;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SeeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SeeApplication.class, args);
-	}
-
-}
+              package bothellcs.SEE;
+              import org.springframework.boot.SpringApplication;
+              import org.springframework.boot.autoconfigure.SpringBootApplication;
+              import org.springframework.web.bind.annotation.GetMapping;
+              import org.springframework.web.bind.annotation.RequestParam;
+              import org.springframework.web.bind.annotation.RestController;
+              
+              @SpringBootApplication
+              @RestController
+              public class SeeApplication {
+                
+                  
+                  public static void main(String[] args) {
+                  SpringApplication.run(SeeApplication.class, args);
+                  }
+                  
+                  @GetMapping("/hello")
+                  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+                  return String.format("Hello %s!", name);
+                  }
+                
+              }
+            
